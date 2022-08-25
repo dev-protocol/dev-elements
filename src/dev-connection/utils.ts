5c2d9fdf7ae2caa0a53e58c5e036b26d7953dba8
aggre <hiroyuki.aggre@gmail.com>
2022-08-25 16:07:55 +0900

    improvement

--- src/dev-connection/utils.ts
+++ src/dev-connection/utils.ts
@@ -1,6 +1,15 @@
+/* eslint-disable functional/no-expression-statement */
+import { define } from '@aggre/ullr'
 import { Connection } from '.'
 
-export const getConnection = (id: string): Connection | undefined => {
-	const el = document.getElementById(id)
-	return el ? (el as Connection) : undefined
+export const getConnection = (id: string): Connection => {
+	define(Connection)
+	const el =
+		getConnection(id) ??
+		((e) => {
+			e.setAttribute('id', id)
+			document.body.appendChild(e)
+			return e
+		})(document.createElement(Connection.is))
+	return el as Connection
 }
